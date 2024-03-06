811cb638546099c6182d4fd071b645235eefe147
Denis Denisov <denis.denisov8701@gmail.com>
2024-03-06 22:29:44 +0100

    validation

--- Efemerides/ViewControllers/Login/LoginViewController.swift
+++ Efemerides/ViewControllers/Login/LoginViewController.swift
@@ -12,20 +12,46 @@ final class LoginViewController: UIViewController {
     @IBOutlet var usernameTF: UITextField!
     @IBOutlet var passwordTF: UITextField!
     
-    private let person = Person.getPerson()
+    private let user = User.getUser()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupUITextFields()
+        
+        usernameTF.delegate = self
+        passwordTF.delegate = self
+        textFieldsUISetup()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        guard usernameTF.text == user.username,
+              passwordTF.text == user.password else {
+            showAlert(
+                withTitle: "Invalid login or password",
+                andMessage: "Please, enter correct login and password") {
+                    guard self.passwordTF.text != "" else { return }
+                    self.passwordTF.text = ""
+                }
+            return false
+        }
+        return true
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let tabBarVC = segue.destination as? TabBarViewController
-        tabBarVC?.person = person
+        tabBarVC?.person = user.person
     }
     
-    // forgot username = tag 0 || forgot password = tag 0
     @IBAction func forgotAction(_ sender: UIButton) {
+        if sender.tag == 0 {
+            showAlert(withTitle: "HI!", andMessage: "username is \(user.username)")
+        } else {
+            showAlert(withTitle: "It secret shh...", andMessage: "password is \(user.password)")
+        }
     }
     
 }
