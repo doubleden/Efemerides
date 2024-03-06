7c81b6efe3644e3e00b845cea2e74e33453e0a69
Denis Denisov <denis.denisov8701@gmail.com>
2024-03-06 21:16:58 +0100

    LoginViewController prepare

--- Efemerides/ViewControllers/Login/LoginViewController.swift
+++ Efemerides/ViewControllers/Login/LoginViewController.swift
@@ -9,10 +9,14 @@ import UIKit
 
 final class LoginViewController: UIViewController {
     
+    @IBOutlet var usernameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
     private let person = Person.getPerson()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUITextFields()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -20,5 +24,21 @@ final class LoginViewController: UIViewController {
         tabBarVC?.person = person
     }
     
+    // forgot username = tag 0 || forgot password = tag 0
+    @IBAction func forgotAction(_ sender: UIButton) {
+    }
+    
 }
 
+private extension LoginViewController {
+    func setupUITextFields() {
+        usernameTF.layer.borderWidth = 2
+        passwordTF.layer.borderWidth = 2
+        
+        usernameTF.layer.cornerRadius = 5
+        passwordTF.layer.cornerRadius = 5
+        
+        usernameTF.layer.borderColor = UIColor(named: "mainYellow")?.cgColor
+        passwordTF.layer.borderColor = UIColor(named: "mainYellow")?.cgColor
+    }
+}
