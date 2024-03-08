60de6b49e79043c8ef08f8d8b23d7807edb8ecb9
Carlos García Pérez <carlosziok@gmail.com>
2024-03-08 03:08:22 +0300

    Installed protocol in WelcomeVC

--- Efemerides/ViewControllers/Welcome/WelcomeViewController.swift
+++ Efemerides/ViewControllers/Welcome/WelcomeViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class WelcomeViewController: UIViewController {
+final class WelcomeViewController: UIViewController, EventSearchable {
     
     var person: Person!
     var events: [Event]!
@@ -22,47 +22,9 @@ final class WelcomeViewController: UIViewController {
         super.viewDidLoad()
         welcomeUserLabel.text = "Welcome, \(person.fullname)"
         birthdayLabel.text = "Your Birthday is on \(person.birthdate)"
-        if let eventInfo = findEvent(for: person.birthdate) {
-            let (title, description, day, month, year) = eventInfo
-            eventsLabel.text = "\(day).\(month).\(year)\n \(title)\n\(description)"
-        }
         
-        func findEvent(
-            for birthdate: String
-        ) -> (
-            title: String,
-            description: String,
-            day: String,
-            month: String,
-            year: String
-        )? {
-            let events = Event.getEvents()
-            
-            let birthdateComponents = birthdate.split(
-                separator: "."
-            )
-            let birthdateDay = String(
-                birthdateComponents[0]
-            )
-            let birthdateMonth = String(
-                birthdateComponents[1]
-            )
-            
-            if let matchingEvent = events.first(where: {
-                event in
-                let eventDay = event.day
-                let eventMonth = event.month
-                return eventDay == birthdateDay && eventMonth == birthdateMonth
-            }) {
-                return (
-                    matchingEvent.title,
-                    matchingEvent.description,
-                    matchingEvent.day,
-                    matchingEvent.month,
-                    matchingEvent.year
-                )
-            }
-            return nil
-        }
+        let event = findEvent(in: events, byDay: person.dayOfBirth, andMonth: person.monthOfBirth)
+        
+        eventsLabel.text = "\(event?.day ?? "").\(event?.month ?? "").\(event?.year ?? "")\n \(event?.title ?? "")\n \(event?.description ?? "")"
     }
 }
