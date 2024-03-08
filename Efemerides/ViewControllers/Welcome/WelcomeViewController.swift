a2a7ac2d749252791174edbab5d915ec320d32b7
Denis Denisov <denis.denisov8701@gmail.com>
2024-03-08 15:20:04 +0100

    debugging

--- Efemerides/ViewControllers/Welcome/WelcomeViewController.swift
+++ Efemerides/ViewControllers/Welcome/WelcomeViewController.swift
@@ -9,23 +9,23 @@ import UIKit
 
 final class WelcomeViewController: UIViewController, EventSearchable {
     
-    var person: Person!
-    var events: [Event]!
-    
     @IBOutlet var welcomeUserLabel: UILabel!
     @IBOutlet var birthdayLabel: UILabel!
     @IBOutlet var eventsLabel: UITextView!
+    @IBOutlet var eventsScrollView: UIScrollView!
+    
+    var person: Person!
+    var events: [Event]!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        eventsScrollView.layer.cornerRadius = 10
+        
         welcomeUserLabel.text = "Welcome, \(person.fullname)!"
         birthdayLabel.text = "Your Birthday is on \(person.birthdate)"
         
         let event = findEvent(in: events, byDay: person.dayOfBirth, andMonth: person.monthOfBirth)
-        
-        eventsLabel.text = "\(event?.day ?? "").\(event?.month ?? "").\(event?.year ?? "")\n\n\(event?.title ?? "")\n\n\(event?.description ?? "")"
-        eventsLabel.layer.cornerRadius = 10
-        eventsLabel.layer.masksToBounds = true
-       
+        eventsLabel.text = "В этот день было - \(event?.title ?? "")\n\n\(event?.description ?? "")"
     }
 }
