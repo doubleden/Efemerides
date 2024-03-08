9c6d1c4f7105f1e1bb3e8e6f2e9d7f9d70fcf9be
Carlos García Pérez <carlosziok@gmail.com>
2024-03-08 13:05:15 +0300

    updated WelcomeVC

--- Efemerides/ViewControllers/Welcome/WelcomeViewController.swift
+++ Efemerides/ViewControllers/Welcome/WelcomeViewController.swift
@@ -20,7 +20,7 @@ final class WelcomeViewController: UIViewController, EventSearchable {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        welcomeUserLabel.text = "Welcome, \(person.fullname)"
+        welcomeUserLabel.text = "Welcome, \(person.fullname)!"
         birthdayLabel.text = "Your Birthday is on \(person.birthdate)"
         
         let event = findEvent(in: events, byDay: person.dayOfBirth, andMonth: person.monthOfBirth)
