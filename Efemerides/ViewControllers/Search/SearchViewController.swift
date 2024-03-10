66087f30b11c8b801a9c4ce4428f68650717f47f
Denis Denisov <denis.denisov8701@gmail.com>
2024-03-10 17:08:42 +0100

    revision constraints

--- Efemerides/ViewControllers/Search/SearchViewController.swift
+++ Efemerides/ViewControllers/Search/SearchViewController.swift
@@ -17,17 +17,11 @@ final class SearchViewController: UIViewController, EventSearchable {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         dateView.layer.cornerRadius = 10
-        dateView.layer.masksToBounds = true
     }
     
     @IBAction func findButtonTapped(_ sender: Any) {
-        guard let selectedDate = datePicker?.date else {
-            print("datePicker is nil or its date property is nil")
-            return
-        }
-        
-        let allEvents = Event.getEvents()
         
         let date = datePicker.date
         let calendar = Calendar.current
@@ -41,7 +35,7 @@ final class SearchViewController: UIViewController, EventSearchable {
         let formattedDay = formatter.string(from: NSNumber(value: day))
         let formattedMonth = formatter.string(from: NSNumber(value: month))
                     
-        foundEvent = findEvent(in: allEvents, byDay: formattedDay ?? "", andMonth: formattedMonth ?? "")
+        foundEvent = findEvent(in: events, byDay: formattedDay ?? "", andMonth: formattedMonth ?? "")
         
         if let foundEvent = foundEvent {
             let storyboard = UIStoryboard(name: "SearchViewController", bundle: nil)
